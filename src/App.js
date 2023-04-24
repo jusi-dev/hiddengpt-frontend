bc451726cd43ab6c1c836b7f49e4dafae31068c3
Justin Winistoerfer <winistoerfer.justin@gmx.ch>
2023-04-24 13:49:52 +0200

    Loosing hope 3.0

--- src/App.js
+++ src/App.js
@@ -7,12 +7,19 @@ import SideNavigation from './components/SideNavigation/SideNavigation';
 import AlphaLogin from './components/AlphaLogin/AlphaLogin';
 
 // Function to add our give data into cache
-const addDataIntoCache = async (cacheName, url, response) => {
-  alert('Adding data into cache')
+const addDataIntoCache = (cacheName, url, response) => {
+  // Converting our response into Actual Response form
   const data = new Response(JSON.stringify(response));
-  const cache = await caches.open(cacheName);
-  await cache.put(url, data);
-  alert('Data added to cache!');
+
+  if ('caches' in window) {
+    // Opening given cache and putting our data into it
+    caches.open(cacheName).then((cache) => {
+      cache.put(url, data);
+      alert('Data Added into cache!')
+    });
+  } else {
+    console.log('caches not in window');
+  }
 };
 
 class App extends React.Component {
